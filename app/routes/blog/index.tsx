3d5e9ffc673142bb3dd7d864787b24080d046d1a
性空山 <sanjin233@gmail.com>
2022-02-11 22:13:54 +0800

    🎉init: blog start

---
+++ app/routes/blog/index.tsx
@@ -0,0 +1,28 @@
+import * as useRemixToBuildBlog from "./use-remix-to-build-blog.mdx";
+import { type LoaderFunction, useLoaderData, Link } from 'remix'
+function postFromModule(mod: any) {
+    return {
+        slug: mod.filename.replace(/\.mdx?$/, ''),
+        ...mod.attributes.meta,
+    }
+}
+
+export const loader: LoaderFunction = () => {
+    return [postFromModule(useRemixToBuildBlog)];
+}
+export default function BlogIndex() {
+    const posts = useLoaderData();
+    return (
+        <div>
+            <h2>Articles</h2>
+            <ul>
+                {posts.map((post: any) => (
+                    <li key={post.slug}>
+                        <Link to={post.slug}>{post.title}</Link>
+                        {post.description ? (<p className="m-0 lg:m-0">{post.description}</p>) : null }
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['./app/**/*.{ts,tsx,js,jsx}'],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [
+        require('@tailwindcss/typography')
+    ],
+};
